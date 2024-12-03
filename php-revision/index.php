09620017ad6b8c0de50f67510428f5f9bb5d7e0c
Abdullah-devsspace-30 <muhammad.abdullah@devsspace.com>
2024-12-03 18:26:07 +0500

    interface implemented

--- php-revision/index.php
+++ php-revision/index.php
@@ -1,37 +1,58 @@
 <?php
 
-class Human {
-    public $gender;
-    public $name;
-    public $age;
-    public $height;
+// class Human {
+//     public $gender;
+//     public $name;
+//     public $age;
+//     public $height;
 
-    function __construct() {
+//     function __construct() {
+//     }
+
+//     function display_details() {
+//         echo "Gender: " . $this->gender . "<br/> Name: " . $this->name . "<br/> Age: " . $this->age . "<br/> Height: " . $this->height . "<br/>";
+//     }
+// }
+
+// class Male extends Human {
+//     public $jd;
+
+//     public function __construct($jd, $gender, $name, $age, $height) {
+//         $this->gender = $gender;
+//         $this->name = $name;
+//         $this->age = $age;
+//         $this->height = $height;
+//         $this->jd = $jd;  
+//     }
+
+//     public function jd_display() {
+//         echo "Gender: " . $this->gender . "<br/> Name: " . $this->name . "<br/> Age: " . $this->age . "<br/> Height: " . $this->height . "<br/> JD: " . $this->jd . "<br/>";
+//     }
+// }
+
+// $obj = new Male("Associate Software Engineer", "Male", "Abdullah Baig", 22, 6.4);
+
+// $obj->jd_display();
+
+interface Animals {
+    public function Animal_Name();
+    public function Animalsound();
+}
+
+class Dog implements Animals {
+    public function Animal_Name() {
+        echo "This is the dog class<br>";
     }
 
-    function display_details() {
-        echo "Gender: " . $this->gender . "<br/> Name: " . $this->name . "<br/> Age: " . $this->age . "<br/> Height: " . $this->height . "<br/>";
+    public function Animalsound() {
+        echo "Dog says bark bark<br>";
     }
 }
 
-class Male extends Human {
-    public $jd;
-
-    public function __construct($jd, $gender, $name, $age, $height) {
-        $this->gender = $gender;
-        $this->name = $name;
-        $this->age = $age;
-        $this->height = $height;
-        $this->jd = $jd;  
-    }
-
-    public function jd_display() {
-        echo "Gender: " . $this->gender . "<br/> Name: " . $this->name . "<br/> Age: " . $this->age . "<br/> Height: " . $this->height . "<br/> JD: " . $this->jd . "<br/>";
-    }
-}
-
-$obj = new Male("Associate Software Engineer", "Male", "Abdullah Baig", 22, 6.4);
-
-$obj->jd_display();
+$dog = new Dog();
+$dog->Animal_Name();  
+$dog->Animalsound();  
 
 ?>
+
+
